6c3e0e0a9c5dd349f67f3e2be1241cb651ad141c
hs-2171098-hyunjinchoi <chju0113@naver.com>
2024-06-08 14:16:15 +0900

    test: AutoConfiguration

---
+++ src/main/java/kr/ac/hansung/cse/hellospringbootsecurity/config/WebConfig.java
@@ -0,0 +1,19 @@
+package kr.ac.hansung.cse.hellospringbootsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+    @Bean
+    public SpringSecurityDialect springSecurityDialect() {
+        return new SpringSecurityDialect();
+    }
+}
